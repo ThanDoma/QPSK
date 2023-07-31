6a4a579525a44328461a802e1b25374f2f0a9d43
ThanDoma <42151643+ThanDoma@users.noreply.github.com>
2023-07-31 19:37:27 +0300

    Delete checking_prohibited_comb.py

--- checking_prohibited_comb.py
+++
@@ -1,8 +0,0 @@
-import ch2
-
-quarter_1_2 = ch2.check_new_1_2
-quarter_1_3 = ch2.check_new_1_3
-quarter_1_4 = ch2.check_new_1_4
-quarter_2_3 = ch2.check_new_2_3
-quarter_2_4 = ch2.check_new_2_4
-quarter_3_4 = ch2.check_new_3_4
